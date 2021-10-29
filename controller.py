ce76fa9825f1b959ceedf1fa937927cf2cc54205
eagarwal-23 <eagarwal@princeton.edu>
2021-10-28 22:46:24 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -30,17 +30,17 @@ def landing():
 @app.route("/profile", methods=["GET"])
 def profile():
     netid = request.args.get("netid")
-    student_info = get_student_info(netid)
+    # student_info = get_student_info(netid)
     html = render_template("profile.html", netid=netid)
 
-    html = render_template("profile.html",
-    name= student_info.get_name(),
-    netid= student_info.get_netid(),
-    classyear= student_info.get_year(),
-    major= student_info.get_major(),
-    clubs= student_info.get_clubs(),
-    bio=student_info.get_bio,
-    interests=student_info.get_interests())
+    # html = render_template("profile.html",
+    # name= student_info.get_name(),
+    # netid= student_info.get_netid(),
+    # classyear= student_info.get_year(),
+    # major= student_info.get_major(),
+    # clubs= student_info.get_clubs(),
+    # bio=student_info.get_bio,
+    # interests=student_info.get_interests())
 
     response = make_response(html)
     return response

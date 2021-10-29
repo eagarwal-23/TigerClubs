9815304b75a7c63012caea82a680817e516dac1e
Anthony Guerra <aguerra013@gmail.com>
2021-10-29 02:17:03 -0400

    pls

--- testingdb.py
+++ testingdb.py
@@ -9,6 +9,7 @@ from sys import argv, stderr, exit
 from psycopg2 import connect, sql
 from contextlib import closing
 from student import Student
+import os
 
 # add database details as constants
 DB = 'd8hudjmal9i0pc'
@@ -17,11 +18,15 @@ PASSWORD = 'e0cb0aca7c7da7773f28d1905455da0f9bf5e83d1a0b98be573e86a621c168e9'
 HOST = 'ec2-23-23-199-57.compute-1.amazonaws.com'
 PORT = '5432'
 
+#DB_URL = 'postgres://rvwhfgtoycqubz:e0cb0aca7c7da7773f28d1905455da0f9bf5e83d1a0b98be573e86a621c168e9@ec2-23-23-199-57.compute-1.amazonaws.com:5432/d8hudjmal9i0pc'
+DATABASE_URL = os.environ['postgres://rvwhfgtoycqubz:e0cb0aca7c7da7773f28d1905455da0f9bf5e83d1a0b98be573e86a621c168e9@ec2-23-23-199-57.compute-1.amazonaws.com:5432/d8hudjmal9i0pc']
+
+
 # given netid and password, return corresponding student info from database
 # for student profile page
 def get_student_infop():
     try:
-        with connect(database=DB, user=USERNAME, password=PASSWORD, host=HOST, port= PORT) as connection:
+        with connect(DATABASE_URL, sslmode = 'require') as connection:
             with closing(connection.cursor()) as cursor:
                 
                 # get student name, netid, year, major, bio

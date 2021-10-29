52e5a505d66bcc50c9b0d4dde1f618fa0cfa3a2f
Anthony Guerra <aguerra013@gmail.com>
2021-10-29 03:02:43 -0400

    qwe

--- testingdb.py
+++ testingdb.py
@@ -34,7 +34,7 @@ def get_student_infop():
             with closing(connection.cursor()) as cursor:
                 
                 # get student name, netid, year, major, bio
-                all = """SELECT * from STUDENT_INFO"""
+                all = "SELECT * from 'tigerclubtest'.'STUDENT_INFO'"
                 cursor = cursor.execute(all)
                 result = cursor.fetchall()
                 print("results:", result)

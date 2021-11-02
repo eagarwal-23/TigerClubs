7731130feba4f4ba8a96be2d9f3bd50ff7f4843b
eagarwal-23 <eagarwal@princeton.edu>
2021-11-01 21:07:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from app import db
-from models import Student, Club, Tag
+from models import Student, Club, Tag, Review
 
 from db1 import student_search
 
@@ -10,13 +10,31 @@ def club_search(search):
     for club in clubs:
         print(club)
 
+<<<<<<< Updated upstream
+=======
+def add_tag(name):
+    tag = Tag(name)
+    db.session.add(tag)
+    db.session.commit()
+
+
+
+>>>>>>> Stashed changes
 if __name__ == "__main__":
-    add_tag('Instruments')
-    add_tag('Movies')
-    add_tag('MCU')
-    add_tag('Baseball')
-    add_tag('Basketball')
-    add_tag('Beachball')
-    add_tag('Sports')
-    add_tag('Bowling')
-    add_tag('Cricket')
+    # add_tag('Instruments')
+    # add_tag('Movies')
+    # add_tag('MCU')
+    # add_tag('Baseball')
+    # add_tag('Basketball')
+    # add_tag('Beachball')
+    # add_tag('Sports')
+    # add_tag('Bowling')
+    # add_tag('Cricket')
+
+    student = Student.query.filter_by(netid = 'eagarwal').first()
+    reviews = student.reviews
+    for review in reviews:
+        print(review)
+
+    # search = 'a'
+    # student_search('a')

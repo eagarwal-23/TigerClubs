50b7884ac69622c5bcc5a5927f928d646bda3c9d
nadiarodriguez00 <78522724+nadiarodriguez00@users.noreply.github.com>
2021-11-03 12:50:21 -0400

    casclient.py

--- casclient.py
+++ casclient.py
@@ -39,7 +39,7 @@ class CasClient:
     # Validate a login ticket by contacting the CAS server. If
     # valid, return the user's username; otherwise, return None.
 
-    def validate(self, ticket):
+        def validate(self, ticket):
         val_url = (self.cas_url + "validate"
             + '?service=' + quote(strip_ticket(request.url))
             + '&ticket=' + quote(ticket))
@@ -50,3 +50,61 @@ class CasClient:
             return None
         first_line = lines[0].decode('utf-8')
         second_line = lines[1].decode('utf-8')
+        if not first_line.startswith('yes'):
+            return None
+        return second_line
+
+    #-------------------------------------------------------------------
+
+    # Authenticate the remote user, and return the user's username.
+    # Do not return unless the user is successfully authenticated.
+
+    def authenticate(self):
+
+        # If the username is in the session, then the user was
+        # authenticated previously.  So return the username.
+        if 'username' in session:
+            return session.get('username')
+
+        # If the request does not contain a login ticket, then redirect
+        # the browser to the login page to get one.
+        ticket = request.args.get('ticket')
+        if ticket is None:
+            login_url = (self.cas_url + 'login?service='
+                + quote(request.url))
+            abort(redirect(login_url))
+
+        # If the login ticket is invalid, then redirect the browser
+        # to the login page to get a new one.
+        username = self.validate(ticket)
+        if username is None:
+            login_url = (self.cas_url + 'login?service='
+                + quote(strip_ticket(request.url)))
+            abort(redirect(login_url))
+
+        # The user is authenticated, so store the username in
+        # the session.
+        session['username'] = username
+        return username
+
+    #-------------------------------------------------------------------
+    # Logout the user.
+
+    def logout(self, next_url):
+
+        # Delete the user's username from the session.
+        session.pop('username')
+
+        # Logout, and redirect the browser to next_url.
+        logout_url = (self.cas_url +  'logout?service='
+            + quote(sub('logout', next_url, request.url)))
+        abort(redirect(logout_url))
+
+#-----------------------------------------------------------------------
+
+def main():
+    print("CasClient does not run standalone")
+
+if __name__ == '__main__':
+    main()
+                                   

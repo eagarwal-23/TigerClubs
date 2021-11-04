894ab68d2d6dd3c19a4a5bd4a27d549fd03fb8d7
nadiarodriguez00 <78522724+nadiarodriguez00@users.noreply.github.com>
2021-11-04 16:50:56 -0400

    logout routing

--- app.py
+++ app.py
@@ -15,14 +15,20 @@ from db1 import get_club_ratings, get_student_info, update_student_info, get_clu
 @app.route("/", methods=["GET"])
 @app.route("/login", methods=["GET"])
 def login():
-    #try:
-    #user = CasClient()
+
     html = render_template("login.html")
     response = make_response(html)
     response.delete_cookie('netid')
     return response
     #except Exception:
         #print("Whoops from login")
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    cas_client = CasClient()
+    cas_client.authenticate()
+    cas_client.logout('landing')
         
 
 @app.route("/admin", methods=["GET"])

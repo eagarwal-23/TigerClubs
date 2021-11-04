0322f6aaa198e09cf8ae169c7c2d4f1746372e06
nadiarodriguez00 <78522724+nadiarodriguez00@users.noreply.github.com>
2021-11-03 23:50:10 -0400

    test

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def landing():
     print(studentname)
     
     print("user = ", auth_user)
-    print("netid = ", len(netid))
+    print("netid", len(netid), "hello")
     user = get_student_info(netid)
     print("user after search is:", user)
     name = user.name

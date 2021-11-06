f4145aa39d69d69e342c2394e52a460926d35629
Natalie Reptak <nreptak@princeton.edu>
2021-11-06 00:29:55 -0400

    Groundwork for Tigerbook - need api key

---
+++ configs.py
@@ -0,0 +1,9 @@
+# Obtained from the following URL:
+# https://tigerbook.herokuapp.com/api/v1/getkey/cos333APIcodeExamples
+# I will integrate this with config.py once I get apikey
+# need to change Key, username, agent
+KEY="1416c1e680eb60f20483e6f377800f80"
+USERNAME="cos333_fall2021"
+AGENT="cos333APIcodeExamples"
+BASE_URL="https://tigerbook.herokuapp.com"
+ALL_UNDERGRADS="/api/v1/undergraduates"

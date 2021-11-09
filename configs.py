8f29c695e4281aac9ecac33b6405143532dbe4bb
Natalie Reptak <nreptak@princeton.edu>
2021-11-09 15:27:45 -0500

    tigrebook test

--- configs.py
+++ configs.py
@@ -2,8 +2,8 @@
 # https://tigerbook.herokuapp.com/api/v1/getkey/cos333APIcodeExamples
 # I will integrate this with config.py once I get apikey
 # need to change Key, username, agent
-KEY="1416c1e680eb60f20483e6f377800f80"
-USERNAME="cos333_fall2021"
-AGENT="cos333APIcodeExamples"
+KEY="87056b7142d9c772cae997436c3fafa4"
+USERNAME="tigerclubs"
+AGENT="tigerclubs"
 BASE_URL="https://tigerbook.herokuapp.com"
 ALL_UNDERGRADS="/api/v1/undergraduates"

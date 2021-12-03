6a33cddfa2dbf5d409e782ab33a98e42b5abd989
nadiarodriguez00 <78522724+nadiarodriguez00@users.noreply.github.com>
2021-12-02 21:53:40 -0500

    g

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ def clubpage():
                                     description = club.description, members = club.members,
                                     tags = club.tags, 
                                     hasScores = True,
-                                    diversity = club.diversity,
+                                    diversity = "{:.1%}".format((club.diversity)),
                                     inclusivity = club.inclusivity,
                                     time_commitment = club.time_commitment,
                                     workload = club.workload,

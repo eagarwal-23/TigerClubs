ea163aa53bedde6482bdb4137de7e7d503aeb568
cnatv16 <cnatv16@gmail.com>
2021-12-04 02:06:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def landing():
     print("aaaaaa")
     print(sort_criteria)
     if not sort_criteria:
-        sort_criteria = 'Overall'
+        sort_criteria = 'combined'
         print("nayyyy")
     else:
         print(sort_criteria)
@@ -291,7 +291,6 @@ def clubpage():
         
         clubname = request.args.get("clubname")
         club = get_club_info(clubname)
-        reviews = get_club_ratings(club.clubid)
 
         html = render_template("clubpage.html", clubname = club.name,
                                     description = club.description, members = club.members,
@@ -302,7 +301,6 @@ def clubpage():
                                     time_commitment = "{:.1%}".format((club.time_commitment/5)),
                                     workload = "{:.1%}".format((club.workload/5)),
                                     experience_requirement = "{:.1%}".format((club.experience_requirement/5)),
-                                    reviews = reviews,
                                     isAdmin = isAdmin)
         response = make_response(html)
         return response
